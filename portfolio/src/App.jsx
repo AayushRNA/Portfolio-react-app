3cdc0f1320655dff1b53ba355631e0a3331b0666
Ayush Rana <ayushranadhoj@gmail.com>
2025-09-28 22:02:25 +1000

    initial draft on portfoilio'

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>My Portfolio</h1>
+      <h2>About Me</h2>
+      <h2>Projects</h2>
+      <h2>Skills</h2>
+      <h2>Contact</h2>
+    </>
+  );
+}
+
+export default App;
